a3873570ad84a4ff5402efb99b09ad5e2e476218
JeremyLuna <jeremylunaalpha@gmail.com>
2018-09-10 13:59:46 -0500

    added mobilenet

---
+++ webapps/mobilenet/mobilenet_script.js
@@ -0,0 +1,32 @@
+// Notice there is no 'import' statement. 'tf' is available on the index-page
+// because of the script tag above.
+
+// learning from:
+// https://www.html5rocks.com/en/tutorials/getusermedia/intro/
+// https://js.tensorflow.org/tutorials/webcam-transfer-learning.html
+
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
+}
+
+if (!hasGetUserMedia()) {
+  alert('getUserMedia() is not supported by your browser');
+} else {
+  document.write("<video autoplay></video>")
+  const video = document.querySelector('video');
+  navigator.mediaDevices.getUserMedia({video:true}).
+  then((stream) => {video.srcObject = stream});
+}
+
+const mobilenet = await tf.loadModel(
+  'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+
+//The input size is [null, 224, 224, 3]
+const input_s = mobilenet.inputs[0].shape;
+
+//The output size is [null, 1000]
+const output_s = mobilenet.outputs[0].shape;
+
+var pred = mobilenet.predict(tf.zeros([1, 224, 224, 3]));
+pred.argMax().print();
