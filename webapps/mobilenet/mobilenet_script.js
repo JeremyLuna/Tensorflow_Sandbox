848d34e66665a9aa099028292d8fe1cb3165dec9
JeremyLuna <jeremylunaalpha@gmail.com>
2018-09-10 15:19:25 -0500

    asdf

--- webapps/mobilenet/mobilenet_script.js
+++ webapps/mobilenet/mobilenet_script.js
@@ -3,34 +3,5 @@
 
 // learning from:
 // https://www.html5rocks.com/en/tutorials/getusermedia/intro/
-// https://js.tensorflow.org/tutorials/webcam-transfer-learning.html
-
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
-}
-
-if (!hasGetUserMedia()) {
-  alert('getUserMedia() is not supported by your browser');
-} else {
-  document.write("<video autoplay></video>")
-  const video = document.querySelector('video');
-  navigator.mediaDevices.getUserMedia({video:true}).
-  then((stream) => {video.srcObject = stream});
-}
-
-// document.write("<p>loading</p>");
-//
-// const mobilenet = await tf.loadModel(
-//   'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
-//
-// document.write("<p>done</p>");
-// //The input size is [null, 224, 224, 3]
-// const input_s = mobilenet.inputs[0].shape;
-//
-// //The output size is [null, 1000]
-// const output_s = mobilenet.outputs[0].shape;
-// document.write("<p>running</p>");
-// var pred = mobilenet.predict(tf.zeros([1, 224, 224, 3]));
-// document.write("<p>done</p>");
-// document.write("<p> ", pred.argMax(), " </p>");
+// https://js.tensorflow.org/tutorials/webcam-transfer-learning.html *
+// https://medium.com/tensorflow/a-gentle-introduction-to-tensorflow-js-dba2e5257702
