e9d78ab24795735ff9320b7663d366a6687589e8
Jeremy Luna <jluna10@islander.tamucc.edu>
2018-09-11 12:39:47 -0500

    BRIGHTNESS WORKS

--- webapps/mobilenet/mobilenet_script.js
+++ webapps/mobilenet/mobilenet_script.js
@@ -1,26 +1,37 @@
-// Notice there is no 'import' statement. 'tf' is available on the index-page
-// because of the script tag above.
-
 // learning from:
 // https://www.html5rocks.com/en/tutorials/getusermedia/intro/
 // https://js.tensorflow.org/tutorials/webcam-transfer-learning.html :
 //      https://github.com/tensorflow/tfjs-examples/tree/master/webcam-transfer-learning
 // https://medium.com/tensorflow/a-gentle-introduction-to-tensorflow-js-dba2e5257702
 
-
-import {Webcam} from './webcam';
-
 // TODO: use
 //      window.screen.availHeight
 //      window.screen.availWidth
 
-const video = document.querySelector('video');
+// stream from camera
+const video = document.getElementById("webcam");
 navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {video.srcObject = stream});
+video.width = 320;
+video.height = 240;
 
-// A webcam class that generates Tensors from the images from the webcam.
-const webcam = new Webcam(document.getElementById('webcam'));
-webcam.setup();
+function calc_brightness(){
+      // Reads the image as a Tensor from the webcam <video> element.
+      net = tf.fromPixels(video);
+      net = tf.mean(net);
+      document.getElementById("brightness").innerHTML = net;
+    //   document.getElementById("brightness").innerHTML = "test";
 
-while (true) {
-    document.getElementById("brightness").innerHTML = "Have a nice day!";
 }
+
+function test_calc_brightness(){
+    image = new ImageData(1, 1);
+    image.data[0] = 100;
+    image.data[1] = 150;
+    image.data[2] = 200;
+    image.data[3] = 255;
+
+    net = tf.fromPixels(image).mean();
+    document.getElementById("brightness").innerHTML = net;
+}
+
+setInterval(calc_brightness, 1000);
