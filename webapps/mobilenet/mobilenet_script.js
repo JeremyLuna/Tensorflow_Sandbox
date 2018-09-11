c45d56130a925b095467ee9364cbb93348429c2c
Jeremy Luna <jeremylunaalpha@gmail.com>
2018-09-10 22:43:25 -0500

    Made Brightness calculator Skeleton

--- webapps/mobilenet/mobilenet_script.js
+++ webapps/mobilenet/mobilenet_script.js
@@ -3,5 +3,16 @@
 
 // learning from:
 // https://www.html5rocks.com/en/tutorials/getusermedia/intro/
-// https://js.tensorflow.org/tutorials/webcam-transfer-learning.html *
+// https://js.tensorflow.org/tutorials/webcam-transfer-learning.html :
+//      https://github.com/tensorflow/tfjs-examples/tree/master/webcam-transfer-learning
 // https://medium.com/tensorflow/a-gentle-introduction-to-tensorflow-js-dba2e5257702
+
+import {Webcam} from './webcam';
+
+// A webcam class that generates Tensors from the images from the webcam.
+const webcam = new Webcam(document.getElementById('webcam'));
+webcam.setup();
+
+while (true) {
+    document.getElementById("brightness").innerHTML = "Have a nice day!";
+}
