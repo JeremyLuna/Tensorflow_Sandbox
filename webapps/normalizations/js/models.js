7fdd5818b0ecf60252a5b6b78b4408773c6553c9
Jeremy Luna <jeremylunaalpha@gmail.com>
2018-09-26 09:19:11 -0500

    added bias to euclidian layer

--- webapps/normalizations/js/models.js
+++ webapps/normalizations/js/models.js
@@ -31,6 +31,7 @@ optimizer = tf.train.sgd(.01);\n\
 
 euclidian_model = "\
 feature = tf.variable(tf.randomNormal(shape=[1, 2, 16], mean=0.0, stdDev=0.2));\n\
+feature_biases = tf.variable(tf.randomNormal(shape=[1, 16], mean=0.5, stdDev=0.2));\n\
 weights = tf.variable(tf.randomNormal(shape=[16, 2], mean=0.0, stdDev=0.2));\n\
 biases = tf.variable(tf.randomNormal(shape=[1, 2], mean=0.5, stdDev=0.2));\n\
 \n\
@@ -39,7 +40,7 @@ predict = function(input){\n\
     net = tf.expandDims(input, 2);\n\
     net = tf.sub(feature, net);\n\
     net = tf.mul(net, net);\n\
-    net = tf.sqrt(tf.sum(net, 1));\n\
+    net = tf.sqrt(tf.sum(net, 1)).add(feature_biases);\n\
     net = tf.relu(net);\n\
     net = tf.matMul(net, weights).add(biases);\n\
     net = tf.relu(net);\n\
