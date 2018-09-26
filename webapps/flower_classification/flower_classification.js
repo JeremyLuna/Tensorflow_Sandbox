5bfe9540cc3d8cad1d84de4a57630fe85319c36d
Jeremy Luna <jeremylunaalpha@gmail.com>
2018-09-26 11:41:59 -0500

    added flower project

---
+++ webapps/flower_classification/flower_classification.js
@@ -0,0 +1,46 @@
+ // learning from
+// saving tf python models:
+//      https://www.tensorflow.org/guide/saved_model#models
+// converting to tf js model:
+//      https://github.com/tensorflow/tfjs-converter
+// loading tf js model
+//      https://js.tensorflow.org/api/0.11.2/#loadModel
+// TODO: video in canvas
+//      https://stackoverflow.com/questions/4429440/html5-display-video-inside-canvas
+//      http://html5doctor.com/video-canvas-magic/
+// TODO: drawing on canvas
+//      https://www.w3schools.com/Html/html5_canvas.asp
+
+// stream from camera
+const video = document.getElementById("webcam");
+// navigator.mediaDevices.getUserMedia({video: true).then((stream) => {video.srcObject = stream});
+// navigator.mediaDevices.getUserMedia({video: { width: 28, height: 28 }}).then((stream) => {video.srcObject = stream});
+
+
+model = "temp";
+let interval_id;
+
+async function setup(){
+    try {
+        model = await tf.loadModel("https://jeremyluna.github.io/Tensorflow_Sandbox/webapps/mnist/saved_model/js_saved_model.json",
+                               strict = false);
+    } catch (err) {
+        console.log("Error: ", err);
+    }
+}
+
+function calc_digit(){
+    // Reads the image as a Tensor from the webcam <video> element.
+    preprocessor = tf.fromPixels(video);
+    preprocessor = tf.mean(preprocessor, 2);
+    preprocessor = tf.expandDims(preprocessor, 0);
+    preprocessor = tf.expandDims(preprocessor, 3);
+    output = model.predict(preprocessor);
+    output = tf.argMax(output, 1);
+    document.getElementById("digit").innerHTML = "Digit: " + output.get([0]);
+    // document.getElementById("digit").innerHTML = output;
+}
+
+setup();
+
+interval_id = setInterval(calc_digit, 1000);
---
+++ webapps/flower_classification/flower_classification_js_model_maker.js
@@ -0,0 +1,257 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+const IMAGE_SIZE = 784;
+const NUM_CLASSES = 10;
+const NUM_DATASET_ELEMENTS = 65000;
+
+const NUM_TRAIN_ELEMENTS = 55000;
+const NUM_TEST_ELEMENTS = NUM_DATASET_ELEMENTS - NUM_TRAIN_ELEMENTS;
+
+const MNIST_IMAGES_SPRITE_PATH =
+    'https://storage.googleapis.com/learnjs-data/model-builder/mnist_images.png';
+const MNIST_LABELS_PATH =
+    'https://storage.googleapis.com/learnjs-data/model-builder/mnist_labels_uint8';
+
+/**
+ * A class that fetches the sprited MNIST dataset and returns shuffled batches.
+ *
+ * NOTE: This will get much easier. For now, we do data fetching and
+ * manipulation manually.
+ */
+class MnistData {
+  constructor() {
+    this.shuffledTrainIndex = 0;
+    this.shuffledTestIndex = 0;
+  }
+
+  async load() {
+    // Make a request for the MNIST sprited image.
+    const img = new Image();
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const imgRequest = new Promise((resolve, reject) => {
+      img.crossOrigin = '';
+      img.onload = () => {
+        img.width = img.naturalWidth;
+        img.height = img.naturalHeight;
+
+        const datasetBytesBuffer =
+            new ArrayBuffer(NUM_DATASET_ELEMENTS * IMAGE_SIZE * 4);
+
+        const chunkSize = 5000;
+        canvas.width = img.width;
+        canvas.height = chunkSize;
+
+        for (let i = 0; i < NUM_DATASET_ELEMENTS / chunkSize; i++) {
+          const datasetBytesView = new Float32Array(
+              datasetBytesBuffer, i * IMAGE_SIZE * chunkSize * 4,
+              IMAGE_SIZE * chunkSize);
+          ctx.drawImage(
+              img, 0, i * chunkSize, img.width, chunkSize, 0, 0, img.width,
+              chunkSize);
+
+          const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+          for (let j = 0; j < imageData.data.length / 4; j++) {
+            // All channels hold an equal value since the image is grayscale, so
+            // just read the red channel.
+            datasetBytesView[j] = imageData.data[j * 4] / 255;
+          }
+        }
+        this.datasetImages = new Float32Array(datasetBytesBuffer);
+
+        resolve();
+      };
+      img.src = MNIST_IMAGES_SPRITE_PATH;
+    });
+
+    const labelsRequest = fetch(MNIST_LABELS_PATH);
+    const [imgResponse, labelsResponse] =
+        await Promise.all([imgRequest, labelsRequest]);
+
+    this.datasetLabels = new Uint8Array(await labelsResponse.arrayBuffer());
+
+    // Create shuffled indices into the train/test set for when we select a
+    // random dataset element for training / validation.
+    this.trainIndices = tf.util.createShuffledIndices(NUM_TRAIN_ELEMENTS);
+    this.testIndices = tf.util.createShuffledIndices(NUM_TEST_ELEMENTS);
+
+    // Slice the the images and labels into train and test sets.
+    this.trainImages =
+        this.datasetImages.slice(0, IMAGE_SIZE * NUM_TRAIN_ELEMENTS);
+    this.testImages = this.datasetImages.slice(IMAGE_SIZE * NUM_TRAIN_ELEMENTS);
+    this.trainLabels =
+        this.datasetLabels.slice(0, NUM_CLASSES * NUM_TRAIN_ELEMENTS);
+    this.testLabels =
+        this.datasetLabels.slice(NUM_CLASSES * NUM_TRAIN_ELEMENTS);
+  }
+
+  nextTrainBatch(batchSize) {
+    return this.nextBatch(
+        batchSize, [this.trainImages, this.trainLabels], () => {
+          this.shuffledTrainIndex =
+              (this.shuffledTrainIndex + 1) % this.trainIndices.length;
+          return this.trainIndices[this.shuffledTrainIndex];
+        });
+  }
+
+  nextTestBatch(batchSize) {
+    return this.nextBatch(batchSize, [this.testImages, this.testLabels], () => {
+      this.shuffledTestIndex =
+          (this.shuffledTestIndex + 1) % this.testIndices.length;
+      return this.testIndices[this.shuffledTestIndex];
+    });
+  }
+
+  nextBatch(batchSize, data, index) {
+    const batchImagesArray = new Float32Array(batchSize * IMAGE_SIZE);
+    const batchLabelsArray = new Uint8Array(batchSize * NUM_CLASSES);
+
+    for (let i = 0; i < batchSize; i++) {
+      const idx = index();
+
+      const image =
+          data[0].slice(idx * IMAGE_SIZE, idx * IMAGE_SIZE + IMAGE_SIZE);
+      batchImagesArray.set(image, i * IMAGE_SIZE);
+
+      const label =
+          data[1].slice(idx * NUM_CLASSES, idx * NUM_CLASSES + NUM_CLASSES);
+      batchLabelsArray.set(label, i * NUM_CLASSES);
+    }
+
+    const xs = tf.tensor2d(batchImagesArray, [batchSize, IMAGE_SIZE]);
+    const labels = tf.tensor2d(batchLabelsArray, [batchSize, NUM_CLASSES]);
+
+    return {xs, labels};
+  }
+}
+
+
+// MODEL DEFINITION
+model = tf.sequential();
+
+model.add(tf.layers.conv2d({
+  inputShape: [28, 28, 1],
+  kernelSize: 5,
+  filters: 8,
+  strides: 1,
+  activation: 'relu',
+  kernelInitializer: 'varianceScaling'
+}));
+
+model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+model.add(tf.layers.conv2d({
+  kernelSize: 5,
+  filters: 16,
+  strides: 1,
+  activation: 'relu',
+  kernelInitializer: 'varianceScaling'
+}));
+
+model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+model.add(tf.layers.flatten());
+
+model.add(tf.layers.dense(
+    {units: 10, kernelInitializer: 'varianceScaling', activation: 'softmax'}));
+
+const LEARNING_RATE = 0.15;
+const optimizer = tf.train.sgd(LEARNING_RATE);
+
+model.compile({
+  optimizer: optimizer,
+  loss: 'categoricalCrossentropy',
+  metrics: ['accuracy'],
+});
+
+const BATCH_SIZE = 64;
+const TRAIN_BATCHES = 150;
+const TEST_BATCH_SIZE = 1000;
+const TEST_ITERATION_FREQUENCY = 5;
+
+async function train() {
+  for (let i = 0; i < TRAIN_BATCHES; i++) {
+    const [batch, validationData] = tf.tidy(() => {
+      const batch = data.nextTrainBatch(BATCH_SIZE);
+      batch.xs = batch.xs.reshape([BATCH_SIZE, 28, 28, 1]);
+
+      let validationData;
+      // Every few batches test the accuracy of the model.
+      if (i % TEST_ITERATION_FREQUENCY === 0) {
+        const testBatch = data.nextTestBatch(TEST_BATCH_SIZE);
+        validationData = [
+          // Reshape the training data from [64, 28x28] to [64, 28, 28, 1] so
+          // that we can feed it to our convolutional neural net.
+          testBatch.xs.reshape([TEST_BATCH_SIZE, 28, 28, 1]), testBatch.labels
+        ];
+      }
+      return [batch, validationData];
+    });
+
+    const history = await model.fit(
+        batch.xs, batch.labels,
+        {batchSize: BATCH_SIZE, validationData, epochs: 1});
+
+    const loss = history.history.loss[0];
+    const accuracy = history.history.acc[0];
+
+    // Call dispose on the training/test tensors to free their GPU memory.
+    tf.dispose([batch, validationData]);
+
+    // tf.nextFrame() returns a promise that resolves at the next call to
+    // requestAnimationFrame(). By awaiting this promise we keep our model
+    // training from blocking the main UI thread and freezing the browser.
+    await tf.nextFrame();
+  }
+}
+
+async function showPredictions() {
+  const testExamples = 100;
+  const batch = data.nextTestBatch(testExamples);
+
+  // Code wrapped in a tf.tidy() function callback will have their tensors freed
+  // from GPU memory after execution without having to call dispose().
+  // The tf.tidy callback runs synchronously.
+  tf.tidy(() => {
+    const output = model.predict(batch.xs.reshape([-1, 28, 28, 1]));
+    const axis = 1;
+    const labels = Array.from(batch.labels.argMax(axis).dataSync());
+    const predictions = Array.from(output.argMax(axis).dataSync());
+  });
+}
+
+let data;
+async function load() {
+  data = new MnistData();
+  await data.load();
+}
+
+// This is our main function. It loads the MNIST data, trains the model, and
+// then shows what the model predicted on unseen test data.
+async function mnist() {
+  await load();
+  console.log("loaded");
+  await train();
+  console.log("trained");
+  showPredictions();
+  console.log("predicted");
+  await model.save("downloads://js_saved_model");
+}
+
+mnist();
