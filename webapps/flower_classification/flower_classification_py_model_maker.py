ae58a5f6133d766e25edad68de3449e056c3c6dd
Jeremy Luna <jeremylunaalpha@gmail.com>
2018-09-30 23:00:21 -0500

    dataset manager works

--- webapps/flower_classification/flower_classification_py_model_maker.py
+++ webapps/flower_classification/flower_classification_py_model_maker.py
@@ -5,31 +5,32 @@
 # TODO:  make flower dataset handler and modify this for flower dataset
 
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
+from flower_dataset import Flower_Dataset
+from scipy import misc
 
-batch_size = 10000
+batch_size = 10
 epochs = 1
 log_level = 2
-mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
+dataset = Flower_Dataset("G:/programming/bina/plant_disease/rose")
 
-x = tf.placeholder('float', [None, 784])
-y = tf.placeholder('float', [None, 10])
+x = tf.placeholder('float', [None, 100, 100, 3])
+y = tf.placeholder('int64', [None])
 
 net = x
-net = tf.reshape(net, [-1, 28, 28, 1])
+net = tf.reshape(net, [-1, 100, 100, 3])
 net = tf.layers.conv2d(net,
     filters = 8,
     kernel_size = 7,
     padding = 'same',
     activation = tf.nn.relu)
-net = tf.reshape(net, [-1, 8*28*28])
+net = tf.reshape(net, [-1, 100*100*8])
 net = tf.layers.dense(net,
     units = 64,
     activation = tf.nn.relu)
 net = tf.layers.dense(net,
-    units = 10, name = "net_out")
+    units = dataset.classes_count, name = "net_out")
 
-loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(y, net))
+loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(tf.one_hot(y, dataset.classes_count), net))
 
 with tf.name_scope('OPTIMIZATION'):
     optimizer = tf.train.AdamOptimizer().minimize(loss)
@@ -37,27 +38,29 @@ with tf.name_scope('OPTIMIZATION'):
 #train
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    steps = int(mnist.train.num_examples/batch_size)
+    steps = int(dataset.train_data_count/batch_size)
     for epoch in range(epochs):
         print("EPOCH: ", epoch+1, "/", epochs)
         for step in range(steps):
-            x_data, y_data = mnist.train.next_batch(batch_size)
+            data = dataset.get_next_train_batch(batch_size)
+            x_data, y_data = data['examples'], data['labels']
             o, l = sess.run([optimizer, loss], feed_dict = {x: x_data, y: y_data})
             if log_level > 0:
                 print("STEP: ", step+1, "/", steps, " STEP LOSS: ", l)
 
     correct_c = 0
-    test_steps = int(mnist.test.num_examples/batch_size)
+    test_steps = int(dataset.test_data_count/batch_size)
     for test_steps in range(test_steps):
-        x_data, y_data = mnist.test.next_batch(batch_size)
-        correct = sess.run(tf.count_nonzero(tf.equal(tf.argmax(net, 1), tf.argmax(y, 1))),
+        data = dataset.get_next_test_batch(batch_size)
+        x_data, y_data = data['examples'], data['labels']
+        correct = sess.run(tf.count_nonzero(tf.equal(tf.argmax(net, 1), y)),
                             feed_dict = {x: x_data, y: y_data})
         correct_c += correct
-    accuracy = correct_c / mnist.test.num_examples
+    accuracy = correct_c / dataset.test_data_count
     print("Test Accuracy: ", accuracy)
 
     # this line shows the names in the nodes, so I can find the node to
     # use as the output in the js model
-    [print(n.name) for n in tf.get_default_graph().as_graph_def().node]
+    # [print(n.name) for n in tf.get_default_graph().as_graph_def().node]
 
     tf.saved_model.simple_save(sess, "py_model_saves", inputs = {"x": x}, outputs = {"net_out": net})
---
+++ webapps/flower_classification/flower_dataset.py
@@ -0,0 +1,79 @@
+# labels are yeilded by path
+# TODO: shuffle
+
+from os import listdir
+from os.path import isfile, join
+from random import shuffle
+import numpy as np
+from skimage.transform import resize
+from scipy import misc
+
+size = (100, 100)
+train_ratio = .7
+
+class Flower_Dataset:
+    data_dir = ""
+
+    classes = [] # in alphabetical order
+    classes_count = 0
+
+    data_count = 0
+    train_data_count = 0
+    test_data_count = 0
+
+    train_data_paths = []
+    test_data_paths = []
+
+    train_data_index = 0
+    test_data_index = 0
+
+    def __init__(self):
+        self.init()
+
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.init()
+
+    def init(self):
+        self.classes = listdir(self.data_dir)
+        self.classes_count = len(self.classes)
+        for a_class in self.classes:
+            example_paths = listdir(self.data_dir + '/' + a_class + '/')
+            example_count = len(example_paths)
+            self.train_data_count = int(train_ratio * example_count)
+            self.test_data_count = example_count - self.train_data_count
+            for file_name_index in range(int(self.train_data_count)):
+                self.train_data_paths.append(a_class + "/" + example_paths[file_name_index])
+            for file_name_index in range(self.train_data_count, example_count):
+                self.test_data_paths.append(a_class + "/" + example_paths[file_name_index])
+        # shuffle
+        shuffle(self.train_data_paths)
+        shuffle(self.test_data_paths)
+
+    def get_next_train_batch(self, examples):
+        data = {'examples': [], 'labels': []}
+        diff = (self.train_data_index + examples) - self.train_data_count
+        if diff <= 0:
+            indexes_to_use = range(self.train_data_index, self.train_data_index + examples)
+        else:
+            indexes_to_use = list(range(self.train_data_index, self.train_data_count)) + list(range(diff))
+        for example in indexes_to_use:
+            im = misc.imread(self.data_dir + '/' + self.train_data_paths[example])
+            im = resize(im/255, size)
+            data['examples'].append(im)
+            data['labels'].append(self.classes.index(self.train_data_paths[example].split('/')[0]))
+        return data
+
+    def get_next_test_batch(self, examples):
+        data = {'examples': [], 'labels': []}
+        diff = (self.test_data_index + examples) - self.test_data_count
+        if diff <= 0:
+            indexes_to_use = range(self.test_data_index, self.test_data_index + examples)
+        else:
+            indexes_to_use = list(range(self.test_data_index, self.test_data_count)) + list(range(diff))
+        for example in indexes_to_use:
+            im = misc.imread(self.data_dir + '/' + self.test_data_paths[example])
+            im = resize(im/255, size)
+            data['examples'].append(im)
+            data['labels'].append(self.classes.index(self.test_data_paths[example].split('/')[0]))
+        return data
