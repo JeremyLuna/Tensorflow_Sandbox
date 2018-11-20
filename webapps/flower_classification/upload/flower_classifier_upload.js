d36a0ddf2dde4d096f44d4d385fa72977390fe22
Jeremy Luna <jeremylunaalpha@gmail.com>
2018-11-19 22:54:43 -0600

    working on upload

--- webapps/flower_classification/upload/flower_classifier_upload.js
+++ webapps/flower_classification/upload/flower_classifier_upload.js
@@ -5,19 +5,6 @@
 //      https://js.tensorflow.org/api/0.13.3/#loadModel
 //      https://simpl.info/getusermedia/sources/
 //      https://js.tensorflow.org/tutorials/model-save-load.html
-// saving tf python models:
-//      https://www.tensorflow.org/guide/saved_model#models
-// converting to tf js model:
-//      https://github.com/tensorflow/tfjs-converter
-// loading tf js model
-//      https://js.tensorflow.org/api/0.11.2/#loadModel actually loadFrozenModel
-// TODO: video in canvas
-//      https://stackoverflow.com/questions/4429440/html5-display-video-inside-canvas
-//      http://html5doctor.com/video-canvas-magic/
-// TODO: drawing on canvas
-//      https://www.w3schools.com/Html/html5_canvas.asp
-// Other dataset "plant village":
-//      https://github.com/spMohanty/PlantVillage-Dataset
 
 // stream from camera
 const video = document.getElementById("webcam");
